d93a7396f5ca873dc83f02b7594d4f571359f801
Gajendran Machakalai <gajendran.machakalai4@outlook.com>
2023-09-27 11:07:10 +0530

    Camera plugin using MediaCapture API

---
+++ WebCameraApp/WebCameraApp/Controllers/CameraController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebCameraApp.Controllers
+{
+    public class CameraController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
